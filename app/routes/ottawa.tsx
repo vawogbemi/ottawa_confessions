7bb6147cca7706a7c36981d06f3d0a30c8985cd0
Victor Awogbemi <vawogbemi@gmail.com>
2024-02-16 06:21:07 -0500

    added carleton ottawa uottawa

---
+++ app/routes/ottawa.tsx
@@ -0,0 +1,51 @@
+import { json, LoaderFunctionArgs, type MetaFunction } from "@remix-run/node";
+import { useFetcher, useLoaderData, useOutletContext } from "@remix-run/react";
+import { fetchPosts, verifyLike } from "~/api/server";
+import { Feed } from "~/components/feed";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export const action = async ({ request }: LoaderFunctionArgs) => {
+  const formData = Object.fromEntries(await request.formData());
+
+  const post = parseInt(formData.post as string);
+  const user = formData.user as string;
+
+  return verifyLike(post, user);
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") || 1;
+
+  const posts = await fetchPosts(request, Number(page), "Ottawa");
+
+  return json({ posts });
+};
+
+export default function Index() {
+  const { posts } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof loader>();
+
+  const { user } = useOutletContext<{
+    user: {
+      id: string;
+      username: string;
+      city: string | null;
+      school: string | null;
+    } | null;
+  }>();
+
+  console.log(posts);
+
+  return (
+    <form className="w-full" method="post">
+      <Feed fetcher={fetcher} posts={posts} user={user} />
+    </form>
+  );
+}
---
+++ app/routes/uottawa.tsx
@@ -0,0 +1,47 @@
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import {
+  Form,
+  useFetcher,
+  useLoaderData,
+  useOutletContext,
+} from "@remix-run/react";
+import { fetchPosts, verifyLike } from "~/api/server";
+import { Feed } from "~/components/feed";
+
+export const action = async ({ request }: LoaderFunctionArgs) => {
+  const formData = Object.fromEntries(await request.formData());
+
+  const post = parseInt(formData.post as string);
+  const user = formData.user as string;
+
+  return verifyLike(post, user);
+};
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") || 1;
+
+  const posts = await fetchPosts(request, Number(page), "uOttawa");
+
+  return json({ posts });
+};
+
+export default function Index() {
+  const { posts } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher<typeof loader>();
+
+  const { user } = useOutletContext<{
+    user: {
+      id: string;
+      username: string;
+      city: string | null;
+      school: string | null;
+    } | null;
+  }>();
+
+  return (
+    <Form className="w-full" method="post">
+      <Feed fetcher={fetcher} posts={posts} user={user} />
+    </Form>
+  );
+}
