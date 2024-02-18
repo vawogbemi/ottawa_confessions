463be010edf58dbe8028cb99437d999e7fd9effc
Victor Awogbemi <vawogbemi@gmail.com>
2024-02-17 20:31:21 -0500

    reimagined site header into main-nav, added desktop nav, re-added action to search, other qol changes

---
+++ app/components/nav-card.tsx
@@ -0,0 +1,36 @@
+import { Link, useLocation } from "@remix-run/react";
+
+export function NavCard(props: {
+    icon: string | JSX.Element | JSX.Element[] | (() => JSX.Element);
+    to: string;
+    user:
+      | {
+          id: string;
+          username: string;
+          city: string | null;
+          school: string | null;
+        }
+      | null
+      | undefined;
+  }) {
+    const pathname = useLocation().pathname;
+    const { icon, user, to } = props;
+    return (
+      <Link
+        className="py-3 flex-1  hover:bg-zinc-100 rounded-md "
+        to={user ? `${to.toLowerCase()}` : "/login"}
+      >
+        <div className="flex">
+          <div
+            className={`mx-auto my-auto ${
+              pathname.endsWith(to.toLowerCase())
+                ? "text-zinc-900"
+                : "text-zinc-400"
+            }`}
+          >
+            {<>{icon}</>}
+          </div>
+        </div>
+      </Link>
+    );
+  }
